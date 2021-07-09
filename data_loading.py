e90de881659a32b8bcc2464e87e443392e0895c3
didillysquat <didillysquat@gmail.com>
2021-07-09 10:20:37 +0200

    Improve lat lon format checking

--- data_loading.py
+++ data_loading.py
@@ -19,7 +19,6 @@ from plotting import DistScatterPlotterSamples, SeqStackedBarPlotter
 from symportal_utils import BlastnAnalysis, MothurAnalysis, NucleotideSequence
 from output import SequenceCountTableCreator
 import ntpath
-import re
 import math
 from numpy import NaN
 from collections import defaultdict
@@ -30,6 +29,7 @@ import sp_config
 from django_general import CreateStudyAndAssociateUsers
 import logging
 import hashlib
+from general import check_lat_lon
 
 
 class DataLoading:
@@ -1106,73 +1106,24 @@ class DataLoading:
 
     def _check_lat_long(self):
         # check the lat long value for each sample listed
-        self.sample_meta_info_df['collection_latitude'] = self.sample_meta_info_df['collection_latitude'].astype(str)
-        self.sample_meta_info_df['collection_longitude'] = self.sample_meta_info_df['collection_longitude'].astype(str)
         for i, sample_name in enumerate(self.sample_meta_info_df.index.values.tolist()):
             lat = self.sample_meta_info_df.at[sample_name, 'collection_latitude']
             lon = self.sample_meta_info_df.at[sample_name, 'collection_longitude']
-            lat = lat.rstrip().lstrip().replace(chr(176), '')
-            lon = lon.rstrip().lstrip().replace(chr(176), '')
 
-            # 1 - Check to see if we are dealing with nan values
-            # any nan values would initially have been numpy.NaN but after convertsion to string above
-            # will be "nan".
-            # This may cause an issue if the column is in str format already
-            if lat == 'nan' or lon == 'nan':
+            try:
+                lat_float, lon_float = check_lat_lon(lat, lon)
+            except Exception:
                 print(f'Lat and long are currently nan for {sample_name}. Values will be set to 999')
                 self._set_lat_lon_to_999(sample_name)
                 continue
+
+            # final check to make sure that the values are in a sensible range
+            if (-90 <= lat_float <= 90) and (-180 <= lon_float <= 180):
+                self.sample_meta_info_df.at[sample_name, 'collection_latitude'] = lat_float
+                self.sample_meta_info_df.at[sample_name, 'collection_longitude'] = lon_float
             else:
-                # try to see if they are compatable floats
-                try:
-                    lat_float = float(lat)
-                    lon_float = float(lon)
-                except Exception:
-                    # see if they are decimal degrees only with the hemisphere anotation of degree sign
-                    try:
-                        if 'N' in lat:
-                            lat_float = float(lat.replace('N', '').replace(chr(176), ''))
-                            # lat_float should be positive
-                            if lat_float < 0:
-                                lat_float = lat_float * -1
-                        elif 'S' in lat:
-                            lat_float = float(lat.replace('S', '').replace(chr(176), ''))
-                            # lat_float should be negative
-                            if lat_float > 0:
-                                lat_float = lat_float * -1
-                        else:
-                            # There was not an N or S found in the lat so we should raise error
-                            raise RuntimeError
-                        if 'E' in lon:
-                            lon_float = float(lon.replace('E', '').replace(chr(176), ''))
-                            # lon_float should be positive
-                            if lon_float < 0:
-                                lon_float = lon_float * -1
-                        elif 'W' in lon:
-                            lon_float = float(lon.replace('W', '').replace(chr(176), ''))
-                            # lon_float should be negative
-                            if lon_float > 0:
-                                lon_float = lon_float * -1
-                        else:
-                            # There was not an N or S found in the lat so we should raise error
-                            raise RuntimeError
-                    except:
-                        # see if they are in proper dms format
-                        try:
-                            lat_float = self.dms2dec(lat)
-                            lon_float = self.dms2dec(lon)
-                        # if all this fails, convert to 999
-                        except Exception:
-                            print(f'Unable to convert the Lat Lon values of {sample_name} to float. '
-                                  f'Values will be set to 999')
-                            self._set_lat_lon_to_999(sample_name)
-                            continue
-                # final check to make sure that the values are in a sensible range
-                if (-90 <= lat_float <= 90) and (-180 <= lon_float <= 180):
-                    self.sample_meta_info_df.at[sample_name, 'collection_latitude'] = lat_float
-                    self.sample_meta_info_df.at[sample_name, 'collection_longitude'] = lon_float
-                else:
-                    self._set_lat_lon_to_999(sample_name)
+                self._set_lat_lon_to_999(sample_name)
+
         # finally make sure that the lat and long cols are typed as float
         self.sample_meta_info_df['collection_latitude'] = self.sample_meta_info_df['collection_latitude'].astype(float)
         self.sample_meta_info_df['collection_longitude'] = self.sample_meta_info_df['collection_longitude'].astype(
@@ -1181,38 +1132,6 @@ class DataLoading:
     def _set_lat_lon_to_999(self, sample_name):
         self.sample_meta_info_df.at[sample_name, 'collection_latitude'] = float(999)
         self.sample_meta_info_df.at[sample_name, 'collection_longitude'] = float(999)
-
-    @staticmethod
-    def dms2dec(dms_str):
-        """Return decimal representation of DMS
-
-            dms2dec(utf8(48°53'10.18"N))
-            48.8866111111F
-
-            dms2dec(utf8(2°20'35.09"E))
-            2.34330555556F
-
-            dms2dec(utf8(48°53'10.18"S))
-            -48.8866111111F
-
-            dms2dec(utf8(2°20'35.09"W))
-            -2.34330555556F
-
-            """
-
-        dms_str = re.sub(r'\s', '', dms_str)
-
-        sign = -1 if re.search('[swSW]', dms_str) else 1
-
-        numbers = [*filter(len, re.split('\D+', dms_str, maxsplit=4))]
-
-        degree = numbers[0]
-        minute = numbers[1] if len(numbers) >= 2 else '0'
-        second = numbers[2] if len(numbers) >= 3 else '0'
-        frac_seconds = numbers[3] if len(numbers) >= 4 else '0'
-
-        second += "." + frac_seconds
-        return sign * (int(degree) + float(minute) / 60 + float(second) / 3600)
 
     def _check_seq_files_exist(self):
         """
